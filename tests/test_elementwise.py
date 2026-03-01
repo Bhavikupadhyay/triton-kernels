583fabf18501aae9561a4f92850fa8420175622b
Bhavik Upadhyay <bhavikupadhyay08@gmail.com>
2026-03-01 01:44:05 -0800

    feat: add ReLU, GELU, SiLU activation kernels

--- tests/test_elementwise.py
+++ tests/test_elementwise.py
@@ -1,5 +1,10 @@
 from kernels.elementwise.vector_add import test_vector_add
+from kernels.elementwise.activations import test_activations
 
 
 def test_vector_add_correctness():
     test_vector_add()
+
+
+def test_activations_correctness():
+    test_activations()
