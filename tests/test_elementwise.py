d0b10c6051a6ab1f7eff9160c6da5074e47b2ea2
Bhavik Upadhyay <bhavikupadhyay08@gmail.com>
2026-03-01 02:16:27 -0800

    feat: add fused bias+activation kernels (relu, gelu, silu)

--- tests/test_elementwise.py
+++ tests/test_elementwise.py
@@ -1,5 +1,6 @@
 from kernels.elementwise.vector_add import test_vector_add
 from kernels.elementwise.activations import test_activations
+from kernels.elementwise.fused_elementwise import test_fused_elementwise
 
 
 def test_vector_add_correctness():
@@ -8,3 +9,7 @@ def test_vector_add_correctness():
 
 def test_activations_correctness():
     test_activations()
+
+
+def test_fused_elementwise_correctness():
+    test_fused_elementwise()
