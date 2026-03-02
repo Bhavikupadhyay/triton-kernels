b89cb39dd5ebaa1466ad1134654184a7a993ad12
Bhavik Upadhyay <bhavikupadhyay08@gmail.com>
2026-03-01 16:20:38 -0800

    feat: add prefix_sum kernel with three-pass parallel scan

---
+++ tests/test_scanning.py
@@ -0,0 +1,11 @@
+from kernels.scanning.prefix_sum import test_prefix_sum
+
+# from kernels.scanning.cummax import test_cummax
+
+
+def test_prefix_sum_correctness():
+    test_prefix_sum()
+
+
+# def test_cummax_correctness():
+#     test_cummax()
