48cc0c626d87a606777387f53f72da17e2b697d2
Bhavik Upadhyay <bhavikupadhyay08@gmail.com>
2026-03-01 21:57:14 -0800

    feat: add naive_matmul kernel (no tiling, baseline)

---
+++ tests/test_matmul.py
@@ -0,0 +1,15 @@
+from kernels.matmul.naive_matmul import test_naive_matmul
+
+# from kernels.matmul.tiled_matmul import test_tiled_matmul
+# from kernels.matmul.batched_matmul import test_batched_matmul
+
+
+def test_naive_matmul_correctness():
+    test_naive_matmul()
+
+
+# def test_tiled_matmul_correctness():
+#     test_tiled_matmul()
+
+# def test_batched_matmul_correctness():
+#     test_batched_matmul()
